e8e5dbe8825cd659e3ef60f9a7c40725d918803b
Francisco Vena <fvena32@gmail.com>
2026-03-04 21:59:38 +0100

    refactor(prettier): remove @prettier/plugin-pug and update configuration for cleaner setup

--- stylelint/index.js
+++ stylelint/index.js
@@ -7,6 +7,18 @@ export default {
     "stylelint-config-recess-order",
   ],
   rules: {
+    "at-rule-no-deprecated": [true, { ignoreAtRules: ["apply"] }],
+    "declaration-block-no-duplicate-properties": [
+      true,
+      {
+        ignore: [
+          "consecutive-duplicates-with-different-values",
+          "consecutive-duplicates-with-different-syntaxes",
+        ],
+      },
+    ],
+    // eslint-disable-next-line unicorn/no-null -- Stylelint requires null to disable a rule
+    "no-descending-specificity": null,
     "no-empty-source": [true, { severity: "warning" }],
     "scss/comment-no-empty": [true, { severity: "warning" }],
     "selector-pseudo-class-no-unknown": [
